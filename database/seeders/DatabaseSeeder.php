4d6cd16e0246f58bc75d4f9771997cfcf8bd023a
ajspryn <adjisupriyono@gmail.com>
2023-03-23 00:11:52 +0700

    butuh syn

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,70 +22,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        // DB::table('users')->insert([
-        //     'name' => 'admin',
-        //     'username' => 'superadmin',
-        //     'email' => 'admin@admin.com',
-        //     'password' => bcrypt('12345678'),
-        //     'avatar' => 'avatar/avatar1.png'
-        // ]);
-
-        // $role = Role::create(['name' => 'Admin']);
-
-        // $permissions = [
-        //     'office',
-        //     'ship',
-        //     'users-list',
-        //     'users-create',
-        //     'users-edit',
-        //     'users-delete',
-        //     'role-list',
-        //     'role-create',
-        //     'role-edit',
-        //     'role-delete',
-        //     'ship-list',
-        //     'ship-create',
-        //     'ship-edit',
-        //     'ship-delete',
-        //     'crew-list',
-        //     'crew-create',
-        //     'crew-edit',
-        //     'crew-delete',
-        //     'Inventory-List',
-        //     'Inventory-Create',
-        //     'Inventory-Edit',
-        //     'Inventory-Delete',
-        //     'Exiting-Data-List',
-        //     'Exiting-Data-Create',
-        //     'Exiting-Data-Edit',
-        //     'Exiting-Data-Delete',
-        //     'Inventory-Stock-List',
-        //     'Inventory-Stock-Create',
-        //     'Inventory-Stock-Edit',
-        //     'Inventory-Stock-Delete',
-        //     'Transaction-In-List',
-        //     'Transaction-In-Create',
-        //     'Transaction-In-Edit',
-        //     'Transaction-In-Delete',
-        //     'Transaction-Out-List',
-        //     'Transaction-Out-Create',
-        //     'Transaction-Out-Edit',
-        //     'Transaction-Out-Delete',
-        //     'Report-Inventory-List',
-        //     'Report-Inventory-Create',
-        //     'Report-Inventory-Edit',
-        //     'Report-Inventory-Delete'
-        // ];
-
-        // foreach ($permissions as $permission) {
-        //     Permission::create(['name' => $permission]);
-        // }
-
-        // $permissions = Permission::pluck('id', 'id')->all();
-
-        // $role->syncPermissions($permissions);
-
-        // User::select()->get()->first()->assignRole([$role->id]);
         DB::table('users')->insert([
             'name' => 'admin',
             'username' => 'superadmin',
@@ -114,7 +50,31 @@ class DatabaseSeeder extends Seeder
             'crew-list',
             'crew-create',
             'crew-edit',
-            'crew-delete'
+            'crew-delete',
+            'Inventory-List',
+            'Inventory-Create',
+            'Inventory-Edit',
+            'Inventory-Delete',
+            'Exiting-Data-List',
+            'Exiting-Data-Create',
+            'Exiting-Data-Edit',
+            'Exiting-Data-Delete',
+            'Inventory-Stock-List',
+            'Inventory-Stock-Create',
+            'Inventory-Stock-Edit',
+            'Inventory-Stock-Delete',
+            'Transaction-In-List',
+            'Transaction-In-Create',
+            'Transaction-In-Edit',
+            'Transaction-In-Delete',
+            'Transaction-Out-List',
+            'Transaction-Out-Create',
+            'Transaction-Out-Edit',
+            'Transaction-Out-Delete',
+            'Report-Inventory-List',
+            'Report-Inventory-Create',
+            'Report-Inventory-Edit',
+            'Report-Inventory-Delete'
         ];
 
         foreach ($permissions as $permission) {
