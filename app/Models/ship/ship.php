c3592baefb234961bbbe45f5167a6cf7e9b20110
ajspryn <adjisupriyono@gmail.com>
2023-03-22 21:08:17 +0700

    wer

--- app/Models/ship/ship.php
+++ app/Models/ship/ship.php
@@ -2,10 +2,19 @@
 
 namespace App\Models\ship;
 
+use App\Models\crew\Crew;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ship extends Model
+class Ship extends Model
 {
     use HasFactory;
+    protected $guarded = [
+        'id'
+    ];
+
+    public function crew()
+    {
+        return $this->hasMany(Crew::class);
+    }
 }
